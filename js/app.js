f9766ab68cfb0fa71b1e4e229af109402474ee92
Yen Xiong Yuan <yenyuan17@gmail.com>
2022-11-02 03:02:16 +0000

    add randomcus function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 
-// TODO: 1 Store the min/max hourly customers, 
+// done: 1 Store the min/max hourly customers,
 // TODO: 2 the average cookies per customer, in object properties.
 // TODO: 3 Use a method of that object to generate a random number of customers per hour. Objects/Math/random
 // TODO: 4 Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated.
@@ -10,41 +10,93 @@
 
 // ! Global Variables + Window into DOM
 
-let shopHours = ['6am','7am','8am','9am','10am','11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let shopHours = [
+  '6am',
+  '7am',
+  '8am',
+  '9am',
+  '10am',
+  '11am',
+  '12pm',
+  '1pm',
+  '2pm',
+  '3pm',
+  '4pm',
+  '5pm',
+  '6pm',
+  '7pm',
+];
 // DOM Manipulation Step 1: Window into the DOM
 // 1st option: document.getElementById ==> method for grabbing tag by its ID
 // 2nd option: document.querySelector ==> first instance of the passed in tag, ID, or class in your HTML
 let seattleSection = document.getElementById('seattle');
-console.dir(seattleSection)
+console.dir(seattleSection);
 
 // ! Helper Function - Utilities
 // get from MDN docs
 // helper function: a function used by another function to get min and max number
-function randomHourlyCustomers (min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+// function randomHourlyCustomers(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+
+
+function randomCookie(customersArr, cookiesSold) {
+  let cookieAvg = [];
+
+  for (let i = 0; i < customersArr.length; i++) {
+    let randomCookie = Math.ceil(customersArr[i] * cookiesSold);
+    cookieAvg.push(randomCookie);
+  }
+
+  return cookieAvg;
 }
 
 // ! Object Literals
 
 let seattle = {
   place: 'Seattle',
-  hourlyCustomers: 0,
-  minHourlyCustomers : 0,
-  maxHourlyCustomers : 0, 
-  averageCookiesPerCustomer : 0,
-  setHourlyCustomers : function(){
-    this.hourlyCustomers = randomHourlyCustomers(23, 65);
-  }
-  
-    render: function() {
+  minHourlyCustomers: 23,
+  maxHourlyCustomers: 65,
+  averageCookiesPerCustomer: 6.3,
+  cust: [],
+  cookies: [],
+
+  setHourlyCustomers: function () {
+    for (let i = 0; i < shopHours.length; i++){
+      let randomCus = Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1) + this.minHourlyCustomers);
+      console.log(randomCus);
+    }
+  },
+
+  render: function () {
+    this.cookies = randomCookie(seattle.cust, seattle.avgCookieSale);
+
     let articleElem = document.createElement('article');
     seattleSection.appendChild(articleElem);
 
     let h2Elem = document.createElement('h2');
     h2Elem.textContent = this.place;
     articleElem.appendChild(h2Elem);
-  }
-}
+
+    let pElem = document.createElement('p');
+    pElem.textContent = `${this.place}`;
+    articleElem.appendChild(pElem);
+
+    let ulElem = document.createElement('ul');
+    articleElem.appendChild(ulElem);
+
+    for (let i = 0; i < shopHours.length; i++) {
+      let liElem = document.createElement('li');
+      liElem.textContent = shopHours[i];
+      ulElem.appendChild(liElem);
+    }
+  },
+};
+
 // ! Executable Code
-seattle.setHourlyCustomers();
+// seattle.setHourlyCustomers();
+// seattle.render();
+
+seattle.cust = randomHourlyCustomers(seattle.minHourlyCustomers, seattle.maxHourlyCustomers);
 seattle.render();
