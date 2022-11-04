0aea36dbaa9669c14046275fc5a64135f44bde48
Yen Xiong Yuan <yenyuan17@gmail.com>
2022-11-04 04:00:06 +0000

    delete and add table row in JS

--- js/app.js
+++ js/app.js
@@ -9,20 +9,20 @@
 // ! Global Variables + Window into DOM
 
 let shopHours = [
-  "6:00am",
-  "7:00am",
-  "8:00am",
-  "9:00am",
-  "10:00am",
-  "11:00am",
-  "12:00pm",
-  "1:00pm",
-  "2:00pm",
-  "3:00pm",
-  "4:00pm",
-  "5:00pm",
-  "6:00pm",
-  "7:00pm",
+  '6:00am',
+  '7:00am',
+  '8:00am',
+  '9:00am',
+  '10:00am',
+  '11:00am',
+  '12:00pm',
+  '1:00pm',
+  '2:00pm',
+  '3:00pm',
+  '4:00pm',
+  '5:00pm',
+  '6:00pm',
+  '7:00pm',
 ];
 // DOM Manipulation Step 1: Window into the DOM
 // 1st option: document.getElementById ==> method for grabbing tag by its ID
@@ -31,6 +31,8 @@ let allStores = [];
 
 let tableElem = document.getElementById('table');
 console.dir(tableElem);
+
+let shopForm = document.getElementById('shop-form');
 
 // ! Helper Function - Utilities
 
@@ -50,39 +52,39 @@ Store.prototype.randomCookies = function () {
 };
 
 Store.prototype.render = function () {
-  let tableRow = document.createElement("tr");
+  let tableRow = document.createElement('tr');
   tableElem.appendChild(tableRow);
-  let city = document.createElement("th");
+  let city = document.createElement('th');
   city.textContent = this.place;
   tableRow.appendChild(city);
 
   for (let i = 0; i < this.cookieNums.length; i++) {
-    let tableData = document.createElement("td");
+    let tableData = document.createElement('td');
     tableData.textContent = this.cookieNums[i];
     tableRow.appendChild(tableData);
   }
-  let totalRow = document.createElement("td");
+  let totalRow = document.createElement('td');
   totalRow.textContent = this.total;
   tableRow.appendChild(totalRow);
 };
 
 function makeHeader() {
-  let tableRow = document.createElement("tr");
+  let tableRow = document.createElement('tr');
   tableElem.appendChild(tableRow);
-  let firstCell = document.createElement("td");
+  let firstCell = document.createElement('td');
   tableRow.appendChild(firstCell);
   for (let i = 0; i < shopHours.length; i++) {
-    let tableData = document.createElement("td");
+    let tableData = document.createElement('td');
     tableData.textContent = shopHours[i];
     tableRow.appendChild(tableData);
   }
 }
 
 function makeFooter() {
-  let tableRow = document.createElement("tr");
-
-  let tableHeader = document.createElement("th");
-  tableHeader.textContent = "Totals";
+  let tableRow = document.createElement('tr');
+  tableRow.id='removeTableRow';
+  let tableHeader = document.createElement('th');
+  tableHeader.textContent = 'Totals';
   tableRow.appendChild(tableHeader);
 
   let grandTotal = 0;
@@ -94,19 +96,20 @@ function makeFooter() {
       hourlyTotal += allStores[j].cookieNums[i];
     }
 
-    let tdElement = document.createElement("th");
+    let tdElement = document.createElement('th');
     tdElement.textContent = hourlyTotal;
     tableRow.appendChild(tdElement);
 
     grandTotal += hourlyTotal;
   }
 
-  let totalElement = document.createElement("th");
+  let totalElement = document.createElement('th');
   totalElement.textContent = grandTotal;
   tableRow.appendChild(totalElement);
 
   tableElem.appendChild(tableRow);
 }
+
 
 
 // ! Constuctor
@@ -121,11 +124,41 @@ function Store (place, min, max, avg) {
   allStores.push(this);
 }
 
-let seattle = new Store("Seattle", 23, 65, 6.3);
-let tokyo = new Store("Tokyo", 3, 24, 1.2);
-let dubai = new Store("Dubai", 11, 38, 3.7);
-let paris = new Store("Paris", 20, 38, 2.3);
-let lima = new Store("Lima", 2, 16, 4.6);
+let seattle = new Store('Seattle', 23, 65, 6.3);
+let tokyo = new Store('Tokyo', 3, 24, 1.2);
+let dubai = new Store('Dubai', 11, 38, 3.7);
+let paris = new Store('Paris', 20, 38, 2.3);
+let lima = new Store('Lima', 2, 16, 4.6);
+
+
+function handleSumit(event) {
+  event.preventDefault();
+
+  let place = event.target.place.value;
+  console.log(place);
+
+  let min = +event.target.min.value;
+  console.log(min);
+
+  let max = +event.target.max.value;
+  console.log(max);
+
+  let avg = +event.target.avg.value;
+  console.log(avg);
+
+  let addStore = new Store(place, min, max, avg);
+  let removeTableRow = document.getElementById('removeTableRow');
+
+  removeTableRow.remove();
+
+  let newPlace = allStores[allStores.length - 1];
+  newPlace.randomCookies();
+  newPlace.render();
+
+  makeFooter();
+
+
+}
 
 // ! Object Literals
 
@@ -152,3 +185,5 @@ lima.randomCookies();
 lima.render();
 
 makeFooter();
+
+shopForm.addEventListener('submit', handleSumit);
